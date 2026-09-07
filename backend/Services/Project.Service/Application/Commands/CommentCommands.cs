d1fdf056ca0e0bc67e4c8521e3f748858e123fb3
Prashant Kumar Verma <prashantkumarlmp209@gmail.com>
2026-09-07 19:02:40 +0530

    refactor: consolidate Comment commands into CommentCommands.cs (Add+Update+Delete co-located like SprintCommands, 1 aggregate = 1 file)

--- backend/Services/Project.Service/Application/Commands/CommentCommands.cs
+++ backend/Services/Project.Service/Application/Commands/CommentCommands.cs
@@ -7,14 +7,28 @@ using Project.Service.Application.Interfaces;
 namespace Project.Service.Application.Commands;
 
 /// <summary>
-/// Comments CRUD - Viewer cannot comment, Client can. Update/Delete only author or OrgAdmin. Handler delegates to ICommentService (MNC-grade DIP).
+/// Comments CRUD - Viewer cannot comment, Client can. Update/Delete only author or OrgAdmin. Co-located in 1 file like SprintCommands.cs:13 (Create/Update/Delete) - 1 aggregate, N commands, not 1 God command.
 /// </summary>
+public record AddCommentCommand(Guid TaskId, string Content, Guid CallerId, List<string> CallerRoles) : IRequest<Result<CommentDto>>;
 public record UpdateCommentCommand(Guid CommentId, string Content, Guid CallerId, List<string> CallerRoles) : IRequest<Result<CommentDto>>;
 public record DeleteCommentCommand(Guid CommentId, Guid CallerId, List<string> CallerRoles) : IRequest<Result<bool>>;
+
+public class AddCommentValidator : AbstractValidator<AddCommentCommand>
+{
+    public AddCommentValidator() { RuleFor(x => x.TaskId).NotEmpty(); RuleFor(x => x.Content).NotEmpty().MaximumLength(5000); }
+}
 
 public class UpdateCommentValidator : AbstractValidator<UpdateCommentCommand>
 {
     public UpdateCommentValidator() { RuleFor(x => x.CommentId).NotEmpty(); RuleFor(x => x.Content).NotEmpty().MaximumLength(5000); }
+}
+
+public class AddCommentHandler : IRequestHandler<AddCommentCommand, Result<CommentDto>>
+{
+    private readonly ICommentService _service;
+    public AddCommentHandler(ICommentService service) => _service = service;
+    public Task<Result<CommentDto>> Handle(AddCommentCommand req, CancellationToken ct)
+        => _service.AddCommentAsync(req.TaskId, req.Content, req.CallerId, req.CallerRoles, ct);
 }
 
 public class UpdateCommentHandler : IRequestHandler<UpdateCommentCommand, Result<CommentDto>>
